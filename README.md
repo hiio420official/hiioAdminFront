e47f671f257560702ac5b0371c2fdbfadf4d75b4
hongsamhc2 <hongsamgcp@gmail.com>
2024-05-22 15:00:46 +0900

    [SETTING]2024-05-22
    To-Do 추가
    작업내역 추가
    react-router-dom 추가
    browserRouter 추가
    path추가

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 # Hiio Admin Client 
+
 ---
+
+## To-Do
+
+---
+-[ ] SignUp page 추가
+-[ ] SignIn page 추가
+-[ ] SignOut page 추가
+-[ ] notfound Page 추가
+-[ ] reset css -> Global Style 추가
+
 ## 작업 내역
+
 ---
+
+
 ### 2024-05-22
+
 ---
-- 프로젝트 생성
-- 소스파일 수정
-- styled-components 추가
+
+-[x] 프로젝트 생성
+-[x] 소스파일 초기화
+-[x] styled-components 추가
 ```shell
 npm install --save styled-components
 ```
+-[x] react-router-dom 추가
+```shell
+npm install --save react-router-dom
+```
+-[x] react-router-dom 추가
+```shell
+npm install --save react-router-dom
+```
+-[x] BrowserRouter 추가
+-[x] SignUp path 추가
+-[x] SignIn path 추가
+-[x] SignOut path 추가
+-[x] notfound path 추가
+ 
+ 
--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import './App.css';
 import styled from "styled-components";
 import {GlobalStyle} from "./libs/styles/GlobalStyle";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <APP>
-      ㅁㄴㅇㅁㄴㅇ
-      <ul>
-        <li>asd</li>
-      </ul>
-        <GlobalStyle/>
-    </APP>
+    <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<div>asdasd</div>}></Route>
+            <Route path={"/signup"} element={<div>asdasd</div>}></Route>
+            <Route path={"/signin"} element={<div>asdasd</div>}></Route>
+            <Route path={"/signout"} element={<div>asdasd</div>}></Route>
+            <Route path={"*"} element={<div>notfound</div>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
-const APP = styled.div`
 
-`
 export default App;
