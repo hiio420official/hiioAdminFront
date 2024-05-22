870575d43db08b861b59ee7203208ad62b93a11e
hongsamhc2 <hongsamgcp@gmail.com>
2024-05-22 14:41:10 +0900

    [INSTALL] Styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {GlobalStyle} from "./libs/styles/GlobalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <APP>
+      ㅁㄴㅇㅁㄴㅇ
+      <ul>
+        <li>asd</li>
+      </ul>
+        <GlobalStyle/>
+    </APP>
   );
 }
+const APP = styled.div`
 
+`
 export default App;
